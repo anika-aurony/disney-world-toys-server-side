0e84a1663506c0c4674971a524a29b75c55a58b6
anikaaurony <aurony111@gmail.com>
2023-05-21 01:37:30 +0600

    single commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
 app.use(cors());
@@ -34,6 +34,13 @@ async function run() {
         res.send(result);
     })
 
+
+    app.get('/toys/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await toysCollection.findOne(query);
+        res.send(result);
+    })
 
     app.post('/toys', async(req, res)=>{
         const toy = req.body;
