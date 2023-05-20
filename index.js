99660c9c53d30cb8c0cc36c5d019469c78bfa7c1
anikaaurony <aurony111@gmail.com>
2023-05-20 22:38:17 +0600

    mongodb commit

--- index.js
+++ index.js
@@ -26,7 +26,21 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const toysCollection = client.db('disneyToys').collection('toys')
+    const toysCollection = client.db('disneyToys').collection('toys');
+
+    app.get('/toys', async(req, res)=>{
+        const cursor = toysCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
+    app.post('/toys', async(req, res)=>{
+        const toy = req.body;
+        console.log(toy);
+        const result = await toysCollection.insertOne(toy);
+        res.send(result)
+    } )
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
